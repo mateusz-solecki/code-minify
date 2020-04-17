6033e7944722b0454df6d0d90f82ca3cfc1158eb
mateusz-solecki <62967142+mateusz-solecki@users.noreply.github.com>
2020-04-17 04:41:09 +0200

    first commit, first versrion of a layout and code structure

---
+++ javascript.js
@@ -0,0 +1,40 @@
+let backup =[];
+
+function start(){
+   let newCode;
+
+
+/*which button is pressed?*/
+   /*minify button*/
+
+   backup =[];
+   newCode = minifyCode(backup);
+/* undo button */
+   newCode = backup;
+
+/* then */
+    displayNewCode(NewCode);
+}
+
+function minifyCode(input){
+
+/* do the actual minification */
+
+return;
+}
+
+function restoreBackup(){
+
+
+
+return;    
+}
+
+
+function displayNewCode(newCode){
+
+}
+
+
+
+start();
